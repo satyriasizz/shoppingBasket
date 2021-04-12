ccf64f584673ebcc71304e49a8aa8d430d8a9ff5
Andrii Lytvynenko <drLitvinenko@gmail.com>
2021-04-12 18:40:40 +0300

    Fixed Soup special offer

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,9 +7,14 @@ import view.Menu
 
 object Main extends App {
 
-  val offersList = List(new ItemOffer(4, 0.9), new ReqOffer(3, 0.5, 2, 2))
+  val offersList = List(
+    new ItemOffer(4, 0.9),
+    new ReqOffer(1, 0.5, 2, 2)
+  )
+
   val basket = new Basket
   val menu = new Menu(basket, offersList)
+
   menu.printMainMenu()
   menu.runSale()
 }
